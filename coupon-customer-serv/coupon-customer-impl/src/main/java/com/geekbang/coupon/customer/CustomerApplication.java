f16a45763a462b796f888936ac11462ad915fd8d
maodou95838 <99202076@qq.com>
2022-07-12 14:21:37 +0800

    master2 init

--- coupon-customer-serv/coupon-customer-impl/src/main/java/com/geekbang/coupon/customer/CustomerApplication.java
+++ coupon-customer-serv/coupon-customer-impl/src/main/java/com/geekbang/coupon/customer/CustomerApplication.java
@@ -3,7 +3,6 @@ package com.geekbang.coupon.customer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
