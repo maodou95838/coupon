bafb29e4ee237876adb706725349b222c63aa488
maodou95838 <99202076@qq.com>
2022-07-13 17:41:33 +0800

    添加nacos依赖

--- coupon-calculation-serv/coupon-calculation-impl/src/main/java/com/geekbang/coupon/calculation/CalculationApplication.java
+++ coupon-calculation-serv/coupon-calculation-impl/src/main/java/com/geekbang/coupon/calculation/CalculationApplication.java
@@ -1,11 +1,18 @@
 package com.geekbang.coupon.calculation;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class CalculationApplication {
     public static void main(String[] args) {
-        SpringApplication.run(CalculationApplication.class, args);
+        try {
+            SpringApplication.run(CalculationApplication.class, args);
+        } catch (Exception e) {
+            log.debug(e.toString());
+            e.printStackTrace();
+        }
     }
 }
