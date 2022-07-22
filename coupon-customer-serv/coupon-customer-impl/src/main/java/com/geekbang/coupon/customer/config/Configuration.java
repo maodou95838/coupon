3043ed10bb8a8c95470ef9721014ea4d98613289
maodou95838 <99202076@qq.com>
2022-07-22 11:36:05 +0800

    fix bug

---
+++ coupon-customer-serv/coupon-customer-impl/src/main/java/com/geekbang/coupon/customer/config/Configuration.java
@@ -0,0 +1,16 @@
+package com.geekbang.coupon.customer.config;
+
+//@org.springframework.context.annotation.Configuration
+public class Configuration {
+
+//    @Bean
+//    @LoadBalanced
+//    public WebClient.Builder register() {
+//        return WebClient.builder();
+//    }
+//
+//    @Bean
+//    Logger.Level feignLogger() {
+//        return Logger.Level.FULL;
+//    }
+}
