8aedcfe45d196472b06078ac3ef2f095fe2429f1
maodou95838 <99202076@qq.com>
2022-07-25 15:11:20 +0800

    add global filter

--- coupon-customer-serv/coupon-customer-impl/src/main/java/com/geekbang/coupon/customer/CustomerApplication.java
+++ coupon-customer-serv/coupon-customer-impl/src/main/java/com/geekbang/coupon/customer/CustomerApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -15,6 +16,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 //@LoadBalancerClient(value = "coupon-template-serv", configuration = CanaryRuleConfiguration.class)
 @EnableFeignClients(basePackages = {"com.geekbang"})
+@EnableJpaAuditing
 public class CustomerApplication {
     public static void main(String[] args) {
         SpringApplication.run(CustomerApplication.class, args);
--- middleware/gateway/src/main/java/com/geekbang/coupon/middleware/gateway/GatewayApplication.java
+++ middleware/gateway/src/main/java/com/geekbang/coupon/middleware/gateway/GatewayApplication.java
@@ -2,11 +2,14 @@ package com.geekbang.coupon.middleware.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"com.geekbang"})
 public class GatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
+
 }
